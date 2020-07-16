2e82003cf2c87db9a4d63a0ce57477ad440af158
TheAdminPro <45514836+TheAdminPro@users.noreply.github.com>
2020-07-16 16:42:53 +0300

    configure language

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import lang from 'element-ui/lib/locale/lang/ru-RU';
+import locale from 'element-ui/lib/locale';
 import App from './App.vue';
 import store from './store';
+
+// configure language
+locale.use(lang);
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
